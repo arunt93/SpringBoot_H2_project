91631bff04b6dcc7244c5f0e5da2130d56a87534
arunt93 <arunthapa.pe@gmail.com>
2026-02-20 22:30:12 +0530

    Proper User, Role and UserRoleMap table design

---
+++ UserManagementSystemApplication/src/main/java/com/project/usermanagement/Service/UserInterface.java
@@ -0,0 +1,16 @@
+package com.project.usermanagement.Service;
+
+import com.project.usermanagement.Domain.User;
+
+import java.util.List;
+
+public interface UserInterface {
+      User getUserById(Long id);
+
+      User saveUser(User user);
+
+      List<User> getAllUser();
+
+      void deleteUser(Long id);
+
+}
